9ad986fd7903303b30fd299acb1628d6b9501e33
maddyhedman <71003013+maddyhedman@users.noreply.github.com>
2021-01-25 11:21:20 +0100

    Completed module

---
+++ src/components/BookList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div> 
+      <h1> {{title}} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+        title: "All Books",
+    }
+}
+}
+</script>
+
+<style scooped>
+h1, h2 {
+    font-weight: normal;
+}
+</style>
