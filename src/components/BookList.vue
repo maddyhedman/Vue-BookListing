10ae0c20ff0a5123365a13688351dc804f100606
maddyhedman <71003013+maddyhedman@users.noreply.github.com>
2021-01-25 12:19:53 +0100

    Completed module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
   <div> 
       <h1> {{title}} 
           <ul>
-              <li v-for="book in books" :key="book in title">{{book.title}}, {{book.author}} </li>
+              <li v-for="book in books">{{book.title}}:{{book.author}} </li>
              
           </ul>
       </h1>
@@ -11,22 +11,17 @@
 
 <script>
 export default {
+    name: 'BookList',
 data() {
     return {
         title: "All Books",
-    }
+   
     books:[
- {
-     title: 'Self-Reliance', author: 'Ralph Waldo Emerson'
-     },
-    {
-        title: 'American Gods', author: 'Neil Gaiman'
-        },
-    {
-        title: 'Amusing Ourselves to Death', author: 'Neil Postman'
-        },
+ {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+    {title: 'American Gods', author: 'Neil Gaiman'},
+    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
     ]
-        
+      }   
     }
 }
  
@@ -37,13 +32,15 @@ data() {
 h1, h2 {
     font-weight: normal;
 }
+
 ul {
     list-style-type: none;
     padding: 0;
 }
+
 li {
     display: block;
-    margin: 0, 10 px;
+    margin: 0 10px;
 
 }
 </style>
