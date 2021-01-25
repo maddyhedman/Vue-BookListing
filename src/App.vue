40c97e396497a5df6836d73656881b8a0e421eb5
maddyhedman <71003013+maddyhedman@users.noreply.github.com>
2021-01-25 11:32:27 +0100

    Completed module

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello-world></hello-world>
+    <book-list></book-list>
   </div>
 </template>
 
 <script>
 import BookList from './components/BookList';
+import Booklist from './components/Booklist.vue';
 
 export default {
   name: 'app',
   components: {
     BookList,
+   
   },
 };
 </script>
